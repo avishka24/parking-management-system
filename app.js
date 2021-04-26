3d6d5a8474e66e334fbe742da4804ca3a64147bd
unknown <gargavishka@gmail.com>
2021-04-26 15:15:28 +0530

    parking management system using javascript

---
+++ app.js
@@ -0,0 +1,10 @@
+const owner = document.getElementById("owner");
+const car = document.getElementById("car");
+const license = document.getElementById("license-plate");
+const entryDate = document.getElementById("entry-date");
+const endDate = document.getElementById("end-date");
+const addCarButton = document.getElementById("add-car-button");
+
+addCarButton.addEventListener('click',() => {
+
+});
